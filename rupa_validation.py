e5c59617a7ba6dbf334c3b594053c2e3f4f4f11e
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2018-05-11 15:51:55 -0500

    Agregando comentarios

--- rupa_validation.py
+++ rupa_validation.py
@@ -6,52 +6,84 @@ from time import clock
 
 # TODO: Solucionar problema con escritura de archivo de salida. Provicional impresion en pantalla
 
+
 def get_file():
+    """
+    Get file with Library of Congress data
+    :return file object
+    """
     loc_file = open("info_unam_loc.txt", "r")
     return loc_file
 
 
 def get_line(file):
+    """
+    Get line by line of file with LOC data
+    :return List of data by line
+    """
     line = file.readline()
     while line:
-        if line[0] is not '|':
-            line = file.readline()
+        if line[0] is not '|':  # Discard all lines that not start with '|'
+            line = file.readline()  # Read next
             continue
         else:
             line = line.split('|')
-            return line
+            return line  # Return list of data fields
     file.close()
     return False  # End of file
 
 
 def strip_special_chars(string):
+    """
+    Remove special chars from an string
+    :param string: Name of worker
+    :return: Name of worker without special chars
+    """
     return ''.join(ch for ch in string if ch not in punctuation + digits)
 
 
 def replace_accents(string):
+    """
+    Remove accents from a string
+    :param string: Name of worker
+    :return: Name of worker without accents
+    """
     return ''.join((c for c in unicodedata.normalize('NFD', string) if unicodedata.category(c) != 'Mn'))
 
 
 def perfect_match(rupa, loc_name):
+    """
+    Verify if a worker name is on RUPA database
+    :param rupa: Iterator that enable RUPA search
+    :param loc_name: Name of worker en LOC file
+    :return: Boolean. True if worker is on RUPA and false if not
+    """
     rupa_info = rupa.__next__()
     loc_name = strip_special_chars(loc_name)
     loc_name = loc_name.upper()
     loc_name = replace_accents(loc_name)
-    if ("UNIVERSIDAD" in loc_name) or ("MEXICO" in loc_name):
+    if ("UNIVERSIDAD" in loc_name) or ("MEXICO" in loc_name):  # To avoid names like "UNIVERDIAD NACIONAL..."
         return False
     while rupa_info:
-        rupa_name = ' '.join(rupa_info[2:5])
+        rupa_name = ' '.join(rupa_info[2:5])  # Transform RUPA name from list to string
         if loc_name == rupa_name:
-            print("PERFECT MATCH-->", loc_name, "=", rupa_name)
+            print("PERFECT MATCH-->", loc_name, "==", rupa_name)
             return True
         else:
             try:
                 rupa_info = rupa.__next__()
             except StopIteration:
-                return False  # try with all rupa workers names
+                return False  # try with all RUPA workers names
 
 
 def adjust_ratio(loc_name, rupa_name, ratio):
+    """
+    Algorithm to adjust the ratio of certainty of the name at RUPA
+    :param loc_name: Name of worker at LOC
+    :param rupa_name: Name of worker at RUPA
+    :param ratio: Current ratio of certainty
+    :return: Adjust ratio
+    """
     delta = 0
     loc_atoms = loc_name.split(' ')
     rupa_atoms = rupa_name.split(' ')
@@ -85,8 +117,8 @@ def proximity(rupa, loc_name):
         seq = SequenceMatcher(None, loc_name, rupa_name)
         ratio = seq.ratio()
         # ratio = adjust_ratio(loc_name, rupa_name, ratio)
-        if ratio >= 0.8999999999:
-            print(str(count) + " PROXIMITY (>= 0.89): " + loc_name + " ~ " + rupa_name)
+        if ratio >= 0.8399999999:
+            print(str(count) + " PROXIMITY (>= 0.839): " + loc_name + " ~ " + rupa_name)
             candidates[rupa_name] = "2"
             try:
                 rupa_info = rupa.__next__()
@@ -96,8 +128,8 @@ def proximity(rupa, loc_name):
                     return candidates  # Dictionary of candidates
                 else:
                     return '0'  # Try all rupa workers names and nothing is found
-        elif ratio >= 0.7999999999:
-            print(str(count) + " PROXIMITY (>= 0.79): " + loc_name + " ~ " + rupa_name)
+        elif ratio >= 0.7399999999:
+            print(str(count) + " PROXIMITY (>= 0.739): " + loc_name + " ~ " + rupa_name)
             candidates[rupa_name] = "3"
             try:
                 rupa_info = rupa.__next__()
